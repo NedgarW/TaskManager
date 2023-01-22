07da5c019fcca5e9d4f97ca82a686ffc1aeedaed
Ildar Nazmutdinov <nedgar98@gmail.com>
2023-01-22 19:56:59 +0300

    project: Compose Basic:  -"Task manager"

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+//import androidx.compose.ui.tooling.data.EmptyGroup.name
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ImageCompleted(
+                        textOne = stringResource(R.string.textOne),
+                        textTwo = stringResource(R.string.textTwo)
+                    )
+                }
+            }
+        }
+    }
+}
+
+//@Composable
+//fun TextCompleted(textOne: String, textTwo: String) {
+//
+//
+//    Text(
+//        text = textOne,
+//        fontWeight = FontWeight.Bold,
+////        textAlign = TextAlign.Center
+//    )
+//
+//    Text(
+//        text = textTwo, fontSize = 18.sp,
+//        fontWeight = FontWeight.Bold,
+////        textAlign = TextAlign.Center
+//
+//    )
+
+
+//}
+
+@Composable
+fun ImageCompleted(textOne: String, textTwo: String) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+
+
+            Image(
+                painter = image, contentDescription = null,
+                modifier = Modifier
+                    .requiredSize(250.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+
+//        TextCompleted(textOne = textOne, textTwo = textTwo)
+
+            Text(
+                text = textOne, fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 24.dp)
+                    .padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = textTwo, fontSize = 16.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TaskManagerTheme {
+        ImageCompleted(
+            textOne = stringResource(R.string.textOne),
+            textTwo = stringResource(R.string.textTwo)
+        )
+    }
+}
